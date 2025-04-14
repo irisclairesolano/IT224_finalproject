e8c474811e910b27fbf6e1b43bc730b668560d40
irisclairesolano <iris.solano@sorsu.edu.ph>
2025-04-14 13:59:23 +0800

    additional pages

--- app/page.js
+++ app/page.js
@@ -2,29 +2,15 @@
 
 'use client'; // Add this line to mark this as a Client Component
 
-import { useState } from 'react';
-import Header from './components/header';
-import Footer from './components/footer';
-import Sidebar from './components/sidebar';
+import Layout from './layout';
 
 export default function Home() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  // Function to toggle sidebar
-  const toggleSidebar = () => setSidebarOpen(!sidebarOpen);
-
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
-      <Header toggleSidebar={toggleSidebar} /> {/* Pass toggleSidebar to Header */}
-      <div className="flex flex-grow relative">
-        <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
-        <main className="flex-grow p-8 mt-16 mb-16 transition-all duration-300 ease-in-out">
-          <h1 className="text-4xl font-bold text-blue-500 text-center">
-            Welcome to My Social App
-          </h1>
-        </main>
-      </div>
-      <Footer />
-    </div>
+    <Layout>
+      <h1 className="text-4xl font-bold text-blue-500 text-center">
+        Welcome to My Social App
+      </h1>
+      {/* You can add more components or content here */}
+    </Layout>
   );
 }
---
+++ app/pages/manage-users.js
@@ -0,0 +1,12 @@
+// app/pages/manage-users.js
+import React from 'react';
+
+const ManageUsers = () => {
+  return (
+    <div>
+      <h1>Manage users </h1>
+    </div>
+  );
+};
+
+export default ManageUsers;
