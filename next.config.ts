6cc166faf90f1def0589a0787d6a8b188d1821b6
irisclairesolano <iris.solano@sorsu.edu.ph>
2025-05-05 20:58:52 +0800

    svg probs

--- next.config.ts
+++ next.config.ts
@@ -1,42 +1,22 @@
 import type { NextConfig } from 'next'
+import path from 'path'
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule: any) =>
-      rule.test?.test?.('.svg')
-    )
-
-    if (fileLoaderRule) {
-      fileLoaderRule.exclude = /\.svg$/i
+  webpack: (config, { isServer }) => {
+    // Resolve path aliases
+    config.resolve = config.resolve || {}
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
     }
 
+    // Add SVG support
+    config.module = config.module || {}
+    config.module.rules = config.module.rules || []
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgo: true,
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'preset-default',
-                  params: {
-                    overrides: {
-                      removeViewBox: false,
-                    },
-                  },
-                },
-                'prefixIds',
-              ],
-            },
-            titleProp: true,
-          },
-        },
-      ],
+      use: ['@svgr/webpack'],
     })
 
     return config
